f36b972bc2dd0b54f2dfcbaa288cc70a8e721aa2
Sai Teja Manchi <saiteja0019@gmail.com>
2023-06-03 22:04:19 +0530

    Added solution to problem 703 of LeetCode

--- problem_703.py
+++ problem_703.py
@@ -1,16 +1,27 @@
 """
 Problem: https://leetcode.com/problems/kth-largest-element-in-a-stream/
 
-Solution: We create a min heap and 
+Solution: We create a min heap based on the initial set fo numbers and then delete the elements that are smaller than the Kth largest element.
+This way our Kth largest element would be the first element in our min heap.
+As we go on adding the new elements we maintain the heap size to be k by popping the smallest element.
+At the end the first element in the heap is the Kth largest element.
+
+Time Complexity: Intially we create min heap using heapify which is a O(m) operation.
+Subsequently we add n elements of the stream to the min heap which would take O(n * log(m + n)).
+So the overall time complexity would be O(n * log(m + n)).
+
+Space Complexity: Since we create a min heap with m + n elements it would be O(m + n).
 """
+import heapq
+
 class KthLargest:
+
     def __init__(self, k: int, nums: List[int]):
-        # minHeap w/ K largest integers
         self.minHeap, self.k = nums, k
         heapq.heapify(self.minHeap)
-        while len(self.minHeap) > k:
+        for i in range(len(nums) - k):
             heapq.heappop(self.minHeap)
-
+        
     def add(self, val: int) -> int:
         heapq.heappush(self.minHeap, val)
         if len(self.minHeap) > self.k:
